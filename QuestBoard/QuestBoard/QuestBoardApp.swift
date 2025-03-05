7319d6a7ca688f3add5209e1b4b19607048c69c5
Mattia <116157101+Tiashi@users.noreply.github.com>
2025-03-05 17:30:39 +0100

    init

---
+++ QuestBoard/QuestBoard/QuestBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestBoardApp.swift
+//  QuestBoard
+//
+//  Created by Mattia on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct QuestBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuestBoard/QuestBoardTests/QuestBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuestBoardTests.swift
+//  QuestBoardTests
+//
+//  Created by Mattia on 05/03/25.
+//
+
+import Testing
+@testable import QuestBoard
+
+struct QuestBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
