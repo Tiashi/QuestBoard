696211fd709d5e7d537fe00becb5658cc42637b4
Mattia <116157101+Tiashi@users.noreply.github.com>
2025-03-06 15:59:56 +0100

    Added Structs class and  QuestDetail

---
+++ QuestBoard/QuestBoard/Structs.swift
@@ -0,0 +1,14 @@
+//
+//  Structs.swift
+//  QuestBoard
+//
+//  Created by Mattia on 06/03/25.
+//
+
+struct Quest {
+    var name: String
+    var description: String
+    var icon: String
+    var difficulty: Int
+    var isUrgent: Bool
+}
---
+++ QuestBoard/QuestBoard/Views/QuestDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  QuestDetailView.swift
+//  QuestBoard
+//
+//  Created by Mattia on 06/03/25.
+//
+
+import SwiftUI
+
+struct QuestDetailView: View {
+    
+    var quest: Quest
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    QuestsView()
+}
