094ed2f85a20ddca92cfcc50717d20e12ea0c628
Rosario d'Antonio <odd3002@gmail.com>
2025-03-06 17:45:48 +0100

    Mi sono giò rotto il cazzo

---
+++ QuestBoard/QuestBoard/Data.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  QuestBoard
+//
+//  Created by Rosario d'Antonio on 06/03/25.
+//
+
+import SwiftUI
+import Observation
+
+@Observable
+class SharedData {
+    
+    var activeQuests: [Quest] = [
+        Quest(name: "String", description: "", icon: "", difficulty: 1, isUrgent: true)
+    ]
+    
+}
+
+var sharedData = SharedData()
--- QuestBoard/QuestBoard/Views/QuestsView.swift
+++ QuestBoard/QuestBoard/Views/QuestsView.swift
@@ -9,17 +9,21 @@ import SwiftUI
 struct QuestsView: View {
     
     var body: some View {
-        ZStack(alignment: .top) {
+        ZStack() {
             Image("background").resizable()
             
-            Text(" Quest Board").font(.custom("Mirage Gothic", size: 30)).padding(.top, 74).foregroundStyle(.white)
+            Text(" Quest Board").font(.custom("Mirage Gothic", size: 30)).padding(.bottom, 620).foregroundStyle(.white)
+            Text("Your Quest Board is looking pretty empty...\n Add a new Quest!").font(.custom("Mirage Gothic", size: 30)).multilineTextAlignment(.center).padding(.bottom, 300).foregroundStyle(.white).frame(width: 270)
+            Image(systemName: "plus.circle").resizable().frame(width: 200, height: 200).foregroundColor(.white)
             VStack {
                 ScrollView() {
                     
                 }.frame(width: 300, height: 570).padding(.top, 120)
             }
         }.ignoresSafeArea(edges: .top)
+
     }
+
 }
 
 #Preview {
