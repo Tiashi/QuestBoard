169512f04fe699a32e46201fcc08ac877dacef1c
Mattia <116157101+Tiashi@users.noreply.github.com>
2025-03-06 16:28:01 +0100

    Update QuestDetailView.swift

--- QuestBoard/QuestBoard/Views/QuestDetailView.swift
+++ QuestBoard/QuestBoard/Views/QuestDetailView.swift
@@ -13,9 +13,47 @@ struct QuestDetailView: View {
     
     var body: some View {
         
+        ZStack {
+            
+            Image("background")
+            
+            ZStack {
+  
+                Image("paper")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 400, height: 500)
+                
+                VStack {
+                    
+                    Text(quest.name)
+                        .font(.largeTitle)
+                        
+                    
+                    Spacer()
+                    
+                    Image(systemName: quest.icon)
+                    Text(quest.description)
+                    
+                    
+                }.frame(width: 400, height: 420)
+                
+                
+                
+            } //QuestPaper
+                .foregroundColor(.white)
+        } //Background
     }
 }
 
 #Preview {
-    QuestsView()
+    QuestDetailView( quest:
+        Quest(
+            name: "Quest Name",
+            description: "Quest description",
+            icon: "star",
+            difficulty: 5,
+            isUrgent: false
+        )
+    )
 }
