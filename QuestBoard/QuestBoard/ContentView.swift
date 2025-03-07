e975cb5bb3c98a312b3e48e1d8c9616f7b3c878e
Mattia <116157101+Tiashi@users.noreply.github.com>
2025-03-07 14:42:50 +0100

    Update ContentView.swift

--- QuestBoard/QuestBoard/ContentView.swift
+++ QuestBoard/QuestBoard/ContentView.swift
@@ -1,22 +1,17 @@
-//  ContentView.swift
-//  QuestBoard
-//
-//  Created by Mattia on 05/03/25.
-//
-
 import SwiftUI
 
+extension Text {
+    func myFont(size customSize: Int) -> some View {
+        return self.font(.custom(
+            "Mirage Gothic",
+            size: CGFloat(customSize)
+        ))
+    }
+}
+
 struct ContentView: View {
-    init() {
-        let itemAppearance = UITabBarItem.appearance()
-        let barAppearance = UITabBar.appearance()
-        barAppearance.backgroundColor = UIColor(.brown)
-        barAppearance.unselectedItemTintColor = UIColor.black
-        UITabBarAppearance().stackedLayoutAppearance.selected.iconColor = UIColor.white
-        
-        itemAppearance.setTitleTextAttributes([ .font: UIFont(name: "Mirage Gothic", size: 14)!], for: .normal)
-        itemAppearance.setTitleTextAttributes([.foregroundColor: UIColor.white, .font: UIFont(name: "Mirage Gothic", size: 14)!], for: .selected)
-        }
+    
+    @State private var selectedTab = 1 //Default tab bar selection (Quests)
     
     var profileView = ProfileView()
     var questsView = QuestsView()
@@ -26,20 +21,38 @@ struct ContentView: View {
         
         TabView() {
             
-            
             profileView.tabItem {
                 Image(systemName: "person.fill")
                 Text("Profile")
-            }.tag(2)
+            }.tag(1)
+            
             questsView.tabItem {
                 Image(systemName: "list.clipboard")
                 Text("Quests")
-            }.tag(1)
+            }.tag(2)
+            
             storeView.tabItem {
                 Image(systemName: "storefront")
                 Text("Store")
             }.tag(3)
             
+        }.onAppear {
+            
+            let barAppearance = UITabBar.appearance()
+            let itemAppearance = UITabBarItem.appearance()
+            
+            let myFont = UIFont( name: "Mirage Gothic", size: 14 )!
+            
+            barAppearance.backgroundColor = UIColor(.brown)
+            barAppearance.unselectedItemTintColor = UIColor(.black)
+            
+            itemAppearance.setTitleTextAttributes([
+                .font: myFont
+            ],for: .normal )
+            itemAppearance.setTitleTextAttributes([
+                .foregroundColor: UIColor.white,
+                .font: myFont
+            ],for: .selected )
         }
         
     }
